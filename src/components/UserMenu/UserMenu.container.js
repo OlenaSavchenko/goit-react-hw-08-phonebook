f1ace91a1caa37c2a58177a61240a6ef3d626d83
Olena Savchenko <olsavchenko1@gmail.com>
2020-08-09 17:50:33 +0300

    fixed avatar

--- src/components/UserMenu/UserMenu.container.js
+++ src/components/UserMenu/UserMenu.container.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import { authSelectors, authOperations } from '../../redux/auth';
 import UserMenu from './UserMenu';
+import defaultAvatar from './defaultAvatar.png';
 
 const mapStateToProps = state => ({
   name: authSelectors.getUsername(state),
+  avatar: defaultAvatar,
 });
 
 const mapDispatchToProps = {
--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -3,8 +3,9 @@ import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 import styles from './UserMenu.module.css';
 
-const UserMenu = ({ name, onLogout }) => (
+const UserMenu = ({ avatar, name, onLogout }) => (
   <div className={styles.container}>
+    <img src={avatar} alt="avatar" width="30" className={styles.avatar} />
     <span className={styles.name}>{name} </span>
     <Button
       type="button"
@@ -20,6 +21,7 @@ const UserMenu = ({ name, onLogout }) => (
 UserMenu.propTypes = {
   name: PropTypes.string.isRequired,
   onLogout: PropTypes.func.isRequired,
+  avatar: PropTypes.string.isRequired,
 };
 
 export default UserMenu;
